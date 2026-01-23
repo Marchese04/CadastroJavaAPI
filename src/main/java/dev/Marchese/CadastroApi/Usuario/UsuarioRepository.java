a9fa0986ad1685d30dee88831221fc5c0f0960dd
Marchese04 <gabrielmarchesed.a@gmail.com>
2026-01-23 17:11:04 -0300

    feat: add extension h2 for my database

--- src/main/java/dev/Marchese/CadastroApi/Usuario/UsuarioRepository.java
+++ src/main/java/dev/Marchese/CadastroApi/Usuario/UsuarioRepository.java
@@ -1,7 +1,6 @@
 package dev.Marchese.CadastroApi.Usuario;
-
-
 import org.springframework.data.jpa.repository.JpaRepository;
+
 //JPA is an abstraction, serves for automatically the query
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
 }
