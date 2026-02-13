bb0933b677fabe524965d0330cab96731ca2cac0
marchese <gabrielmarchesed.a@gmail.com>
2026-02-13 18:20:41 -0300

    Fix: I started moving the control layer

--- src/main/java/dev/Marchese/CadastroApi/Usuario/UsuarioController.java
+++ src/main/java/dev/Marchese/CadastroApi/Usuario/UsuarioController.java
@@ -1,8 +1,6 @@
 package dev.Marchese.CadastroApi.Usuario;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,6 +9,36 @@ public class UsuarioController {
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Essa é minha primeira mensagem nessa rota, Hello World!!";
+    }
+
+    //Creat user
+    @PostMapping("/CreatUser")
+    public String CreateUser (){
+        return "Creat user";
+    }
+
+    //Show all users
+    @GetMapping("/ShowUser")
+    public String showUsers(){
+        return "This is all users";
+    }
+
+    //Show users by id
+    @GetMapping("/ShowUserId")
+    public String showUserId(){
+        return "This user with id";
+    }
+
+    //Change user data
+    @PutMapping("/changeUser")
+    public String changeUser(){
+        return "Choose the user you want to change";
+    }
+
+    //Delete user by id
+    @DeleteMapping("/deleteUser")
+    public String deleteUser(){
+        return "User deleted successfully ";
     }
 
 }
